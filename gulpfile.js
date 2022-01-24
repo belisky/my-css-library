c3da56b365db71998e762942aa2835f49bfdd82f
bel <denoblesnobility2@gmail.com>
2022-01-24 13:12:21 +0000

    added a gulp purgecss to remove unwanted css classes hence returning a smaller css library size

--- gulpfile.js
+++ gulpfile.js
@@ -1,14 +1,16 @@
 const {src,dest,watch,series}=require('gulp')
-const sass = require('gulp-sass')(require('sass'))
+const sass = require('gulp-sass')(require('sass'));
+const purgecss=require('gulp-purgecss')
 
 function buildStyle() {
     return src('belcss/**/*.scss')
         .pipe(sass())
+        .pipe(purgecss({content:['*.html']}))
     .pipe(dest('css'))
 }
 
 function watchTask() {
-    watch(['belcss/**/*.scss'],buildStyle)
+    watch(['belcss/**/*.scss','*.html'],buildStyle)
 }
 
 
